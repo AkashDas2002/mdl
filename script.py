f311b005a202271efd22e973a4fcedd0c603c3a6
Akash Das <akashdas.ny@gmail.com>
2020-05-02 19:14:45 -0400

    edited file

--- script.py
+++ script.py
@@ -46,6 +46,76 @@ def run(filename):
                           'blue': [0.2, 0.5, 0.5]}]
     reflect = '.white'
 
-    print(symbols)
+    # print(symbols)
+    # print("*************************************")
+    # print(stack)
     for command in commands:
-        print(command)
+        # print(command)
+        if command['op'] == 'push':
+            stack.append([x[:] for x in stack[-1]])
+
+        elif command['op'] == 'pop':
+            stack.pop()
+
+        elif command['op'] == 'move':
+            translate = make_translate(float(command['args'][0]), float(command['args'][1]), float(command['args'][2]))
+            matrix_mult(stack[-1], translate)
+            stack[-1] = [x[:] for x in translate]
+
+        elif command['op'] == 'rotate':
+            if command['args'][0] == 'x':
+                rotate = make_rotX(command['args'][1] * math.pi / 180)
+            elif command['args'][0] == 'y':
+                rotate = make_rotY(command['args'][1] * math.pi / 180)
+            else:
+                rotate = make_rotZ(command['args'][1] * math.pi / 180)
+
+            matrix_mult(stack[-1], rotate)
+            stack[-1] = [x[:] for x in rotate]
+
+        elif command['op'] == 'scale':
+            scale = make_scale(float(command['args'][0]), float(command['args'][1]), float(command['args'][2]))
+            matrix_mult(stack[-1], scale)
+            stack[-1] = [x[:] for x in scale]
+
+        elif command['op'] == 'box':
+            add_box(coords1, float(command['args'][0]), float(command['args'][1]), float(command['args'][2]),
+            float(command['args'][3]), float(command['args'][4]), float(command['args'][5]))
+            matrix_mult(stack[-1], coords1)
+            if command['constants']:
+                draw_polygons(coords1, screen, zbuffer, view, ambient, light, symbols, command['constants'])
+            else:
+                draw_polygons(coords1, screen, zbuffer, view, ambient, light, symbols, '.white')
+            coords1 = []
+
+        elif command['op'] == 'sphere':
+            add_sphere(coords1, float(command['args'][0]), float(command['args'][1]), float(command['args'][2]), float(command['args'][3]), step_3d)
+            matrix_mult(stack[-1], coords1)
+            if command['constants']:
+                draw_polygons(coords1, screen, zbuffer, view, ambient, light, symbols, command['constants'])
+            else:
+                draw_polygons(coords1, screen, zbuffer, view, ambient, light, symbols, '.white')
+            coords1 = []
+
+        elif command['op'] == 'torus':
+            add_torus(coords1, float(command['args'][0]), float(command['args'][1]), float(command['args'][2]),
+            float(command['args'][3]), float(command['args'][4]), step_3d)
+            matrix_mult(stack[-1], coords1)
+            if command['constants']:
+                draw_polygons(coords1, screen, zbuffer, view, ambient, light, symbols, command['constants'])
+            else:
+                draw_polygons(coords1, screen, zbuffer, view, ambient, light, symbols, '.white')
+            coords1 = []
+
+        elif command['op'] == 'line':
+            add_edge(coords, float(command['args'][0]), float(command['args'][1]), float(command['args'][2]),
+            float(command['args'][3]), float(command['args'][4]), float(command['args'][5]))
+            matrix_mult(stack[-1], coords)
+            draw_lines(coords, screen, zbuffer, color)
+            coords = []
+
+        elif command['op'] == 'save':
+            save_extension(screen, command['args'][0] + '.png')
+
+        elif command['op'] == 'display':
+            display(screen)
